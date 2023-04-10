9136f180c94e3d05879a34a800541a51b22fe263
Liqing(lee-ching) Li <53241017+Liqingcathy@users.noreply.github.com>
2023-04-10 10:09:54 -0700

    Update app.py

--- app/app.py
+++ app/app.py
@@ -16,7 +16,6 @@ carbon_key = os.environ.get("CARBON_INTF_API_KEY")
 
 # app = create_app()
 
-# header for carbon api
 HEADER = {'Authorization': f'Bearer {carbon_key}',
           'Content-Type': 'application/json'}
 
@@ -26,7 +25,6 @@ def get_vehicle_model_id(id):
     '''data: vehicle_model_id,vehicle_model_name, vehicle_brand_name(limited to 4), year '''
     url = f"https://www.carboninterface.com/api/v1/vehicle_makes/{id}/vehicle_models"
     model_list = requests.get(url, headers=HEADER).json()
-
     return model_list
 
 # calls from frontend to create user's estimation result
@@ -36,7 +34,7 @@ def create_estimated_val():
         'https://www.carboninterface.com/api/v1/vehicle_makes', headers=HEADER)).json()
     request_body = request.get_json()
     vehicle_make_id = None
-    # get vehicle make id by calling api func
+
     for i in range(len(list_makes)):
         for val in list_makes[i].values():
             if request_body['brand_name'] == val['attributes']['name']:
@@ -47,14 +45,13 @@ def create_estimated_val():
 
     if not vehicle_model_list:
         return jsonify("Sorry, We couldn't find your model.", 404)
+    
     for i in range(len(vehicle_model_list)):
         for val in vehicle_model_list[i].values():
             if val['attributes']['name'] == request_body['model_name']:
                 if int(val['attributes']['year']) == int(request_body['year']):
                     vehicle_model_id = val['id']
                     break
-                    #vehicle_name = val['attributes']['name']
-                    #print(f'model and id match? {vehicle_model_id} {vehicle_name}')
 
     request_body['vehicle_model_id'] = vehicle_model_id
 
@@ -69,23 +66,10 @@ def create_estimated_val():
             int(request_body['emission']) // int(request_body['distance_value']))
 
         # verify_name = request_body['user_name']
-        # verify duplication of user name before creating new record
         # res = es.search(index='user_input', body=json.dumps(
         #     {"query": {"match_phrase": {"user_name": verify_name}}}))  # exact search
 
+        # "create id when not exists"
         # if len(res['hits']['hits']) == 0:
-        #     print("create id when not exists")
         es.index(index='user_input', body=request_body)
     return response, 201
-
-
-# @app.route('/')
-# def hello():
-#     return 'Welcome to Carbon emission server'
-
-
-# if __name__ == '__main__':
-#     # app.run()
-
-#     # app.run(port=5000, debug=True)
-#     app.run(host='0.0.0.0', port=5000, debug=True)
